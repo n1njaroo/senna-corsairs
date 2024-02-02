c5d9877c0198b693776f40a568bb8ec401f6c32f
kenpisig27 <57336264+n1njaroo@users.noreply.github.com>
2024-02-03 03:09:53 +0800

    initial implementation of ticket support system

--- src/commands/slash/Developers/ticket.js
+++ src/commands/slash/Developers/ticket.js
@@ -1,139 +1,88 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
 const {
-  SlashCommandBuilder,
-  EmbedBuilder,
-  ActionRowBuilder,
-  StringSelectMenuBuilder,
   ChannelType,
   PermissionsBitField,
+  EmbedBuilder,
 } = require("discord.js");
-const ticket = require("../../../schemas/TicketSchema");
+
+// Function to generate a random string as a unique identifier
+function generateUniqueIdentifier() {
+  return Math.random().toString(36).substring(2, 8);
+}
 
 module.exports = {
   structure: new SlashCommandBuilder()
     .setName("ticket")
-    .setDescription("Manage the ticket system")
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("send")
-        .setDescription("Send the ticket message")
-        .addStringOption((option) =>
-          option
-            .setName("name")
-            .setDescription("The name for the open select menu content")
-            .setRequired(true)
-        )
-        .addStringOption((option) =>
-          option
-            .setName("message")
-            .setDescription("A custom message to add the embed")
-            .setRequired(false)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("setup")
-        .setDescription("Setup the ticket category")
-        .addChannelOption((option) =>
-          option
-            .setName("category")
-            .setDescription("The category to set up the ticket system")
-            .addChannelTypes(ChannelType.GuildCategory)
-            .setRequired(true)
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand.setName("remove").setDescription("Close the ticket")
-    )
-    .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator),
+    .setDescription(
+      "Create a support ticket to get better assistance regarding your issue."
+    ),
+
+  options: {
+    cooldown: 3000,
+  },
+
+  event: "interactionCreate",
 
   run: async (client, interaction, args) => {
-    try {
-      const { options } = interaction;
-      const sub = options.getSubcommand();
-      const data = await ticket.findOne({ Guild: interaction.guild.id });
+    const user = interaction.user;
+    const uniqueIdentifier = generateUniqueIdentifier();
+    const channelName = `🎫︱ticket-${uniqueIdentifier}`;
 
-      switch (sub) {
-        case "send":
-          if (!data)
-            return await interaction.reply({
-              content:
-                "The ticket system is not set up yet. Do /ticket setup before you can send a ticket",
-              ephemeral: true,
-            });
+    const channel = await interaction.guild.channels.create({
+      name: channelName,
+      type: ChannelType.GuildText,
+      parent: "1198605134729584750",
+      permissionOverwrites: [
+        // Allow the user to view and send messages
+        {
+          id: user.id,
+          allow: [
+            PermissionsBitField.Flags.ViewChannel,
+            PermissionsBitField.Flags.SendMessages,
+          ],
+        },
+        // Deny access for everyone else
+        {
+          id: interaction.guild.roles.everyone.id,
+          deny: [PermissionsBitField.Flags.ViewChannel],
+        },
+      ],
+    });
 
-          const name = options.getString("name");
-          const message =
-            options.getString("message") ||
-            "Create a ticket to talk with the server staff! Once you selected below, use the input to describe why you are creating a ticket";
+    if (channel) {
+      // Send an embed message to the user's ticket channel
+      const embedMessage = new EmbedBuilder()
+        .setColor("#3498db")
+        .setTitle("Ticket Created")
+        .setDescription(
+          `Thank you for creating a ticket, ${user.username}! A support representative will be with you shortly.`
+        );
 
-          const select = new ActionRowBuilder().addComponents(
-            new StringSelectMenuBuilder()
-              .setCustomId("ticketCreateSelect")
-              .setPlaceholder(name)
-              .setMinValues(1)
-              .addOptions({
-                label: "Create your ticket",
-                description: "Click to begin the ticket creation process",
-                value: "createTicket",
-              })
+      await channel.send({ embeds: [embedMessage] });
+
+      // Send notification to the support channel
+      const supportChannel = interaction.guild.channels.cache.get(
+        "1198629640718274600"
+      );
+
+      if (supportChannel) {
+        const notificationEmbed = new EmbedBuilder()
+          .setColor("#ff0000")
+          .setTitle("New Ticket")
+          .setDescription(
+            `${user.username} has created a new ticket. Click below to join.`
           );
 
-          const embed = new EmbedBuilder()
-            .setColor("Random")
-            .setTitle("Create a ticket")
-            .setDescription(message);
-
-          await interaction.reply({
-            content: "I have sent your ticket message below",
-            ephemeral: true,
-          });
-          await interaction.followUp({
-            embeds: [embed],
-            components: [select],
-          });
-          break;
-
-        case "remove":
-          if (!data)
-            return await interaction.reply({
-              content:
-                "The ticket system is not set up yet. Do /ticket setup before you can remove a ticket",
-              ephemeral: true,
-            });
-          else {
-            await ticket.deleteOne({ Guild: interaction.guild.id });
-            await interaction.reply({
-              content: "The ticket system has been removed",
-              ephemeral: true,
-            });
-          }
-          break;
-
-        case "setup":
-          if (data)
-            return await interaction.reply({
-              content: `Looks like you already have a ticket category set to <#${data.Category}>`,
-              ephemeral: true,
-            });
-          else {
-            const category = options.getChannel("category");
-            await ticket.create({
-              Guild: interaction.guild.id,
-              Category: category.id,
-            });
-            await interaction.reply({
-              content: `The ticket system has been set up in the category **<${category}>**`,
-              ephemeral: true,
-            });
-          }
-          break;
+        await supportChannel.send({
+          embeds: [notificationEmbed],
+        });
+      } else {
+        console.error("Support channel not found!");
       }
-    } catch (error) {
-      console.error("An error occurred:", error);
-      await interaction.reply({
-        content: "An error occurred while processing your request.",
-        ephemeral: true,
-      });
+
+      await interaction.reply("Ticket created successfully!");
+    } else {
+      console.error("Failed to create ticket channel for user:", user.id);
     }
   },
 };
--- src/schemas/TicketSchema.js
+++ src/schemas/TicketSchema.js
@@ -1,8 +1,23 @@
-const { Schema, model } = require("mongoose");
+const { model, Schema } = require("mongoose");
 
-let ticketSchema = new Schema({
-  Guild: String,
-  Category: String,
-});
-
-module.exports = model("ticketsys100110", ticketSchema);
+// Define the TicketSchema
+module.exports = model(
+  "TicketSchema",
+  new Schema({
+    // User ID associated with the ticket
+    userId: {
+      type: String,
+      required: true, // User ID is required
+    },
+    // Channel ID associated with the ticket
+    channelId: {
+      type: String,
+      required: true, // Channel ID is required
+    },
+    // Array of messages in the ticket
+    messages: {
+      type: Array,
+      required: true, // Messages array is required
+    },
+  })
+);
